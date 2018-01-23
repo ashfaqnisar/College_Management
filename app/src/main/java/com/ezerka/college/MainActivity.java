2440c20da79db8f96ee95ec2cfa78985b82b291b
ashfaqnisar <ashfaqnisar00@gmail.com>
2018-01-24 02:36:28 +0530

    Version 4-
    1.Added All the activities required
    2.Added all the settings file to it
    3.added all the connection files from one activity to another activiy

--- app/src/main/java/com/ezerka/college/MainActivity.java
+++ app/src/main/java/com/ezerka/college/MainActivity.java
@@ -14,35 +14,70 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
-    public void attendance(View view) {
+    public void track_activity(View view) {
+        startActivity(new Intent(this, TrackActivity.class));
 
+    }
+
+    public void attendance_activity(View view) {
+        startActivity(new Intent(this, AttendanceActivity.class));
+    }
+
+    public void results_activity(View view) {
+        startActivity(new Intent(this, ResultsActivity.class));
+
+    }
+
+    public void notices_activity(View view) {
+        startActivity(new Intent(this, NoticesActivity.class));
+
+    }
+
+    public void clubs_activity(View view) {
+        startActivity(new Intent(this, ClubsActivity.class));
+
+    }
+
+    public void anti_ragging_activity(View view) {
+        startActivity(new Intent(this, AntiRaggingActivity.class));
+
+    }
+
+    public void faculty_activity(View view) {
+        startActivity(new Intent(this, FacultyActivity.class));
+
+    }
+
+    public void payment_activity(View view) {
+        startActivity(new Intent(this, PaymentActivity.class));
+
+    }
+
+    public void forums_activity(View view) {
+        startActivity(new Intent(this, ForumsActivity.class));
+    }
+
+    public void contact_us_activity(View view) {
+        startActivity(new Intent(this, ContactActivity.class));
     }
 }
